e04435891b47d0334712401572ff5b164d5fe496
Yont <56182726+yontwises@users.noreply.github.com>
2020-01-09 01:13:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,21 @@ using namespace std;
 
 
 int main() {
-  // std::cout << "Hello World! " << '\n';
-  // Playlist list;
-  PlaylistNode* testNode = new PlaylistNode;
-  string testID = "OPGG";
-  string testName = "Kill This Love";
-  string testArtist = "Blackpink";
-  int testLength = 300;
-  // testNode->InsertAfter(testID, testName, testArtist, testLength);
+	// std::cout << "Hello World! " << '\n';
+	// Playlist list//
+	Playlist list;
 
-  // PlaylistNode* curr = head;
-  // while (curr != nullptr) {
-  //   PrintPlaylistNode(curr);
-  //   cout << endl;
-  //   curr = curr->nextNodePtr;
-  // }
+	list.AddSong("H103", "DuDu", "Blackpink", 340);
+	list.AddSong("H523", "Kill This Love ", "Blackpink", 200);
+	list.AddSong("H523", "Kill This Love ", "Blackpink", 200);
+	list.AddSong("H523", "Kill This Love ", "Blackpink", 200);
+	/*list.OutputFullPlayList();*/
+	list.RemoveSong("H523");
+	list.OutputFullPlayList();
+	// testNode->InsertAfter(testID, testName, testArtist, testLength);
 
-  return 0;
+	
+
+	return 0;
 
 }
